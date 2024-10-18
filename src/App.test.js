2ffd2dbb05987c78b6386c7f9f9907aad9ee5a04
Kim.Chanho <kiss.yagni.dry@gmail.com>
2024-10-18 18:04:56 +0900

    init project

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import {describe, it, expect} from "vitest";
+
+describe('App.vue', () => {
+    it('test 1 is 1', () => {
+        expect(1).toEqual(1);
+    })
+})
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<!--<script>-->
+<!--export default {-->
+<!--  data () {-->
+<!--    return {-->
+<!--      userId : '',-->
+<!--      userPw : '',-->
+<!--      sex: 'male'-->
+<!--    }-->
+<!--  },-->
+<!--  methods: {-->
+<!--    login () {-->
+<!--      alert(`id: ${this.userId} / pw: ${this.userPw} / sex: ${this.sex}`)-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+<script setup>
+  import { ref } from 'vue'
+
+  const user = ref({
+    id: '',
+    pw: '',
+    sex: 'male'
+  })
+
+  const login = () => {
+    alert(`id: ${user.value.id} / pw: ${user.value.pw} / sex: ${user.value.sex}`)
+  }
+
+  const onInputHandler = (e) => {
+    const attr = e.target.getAttribute("id");
+    const key = attr.substring(attr.lastIndexOf('.')+1);
+    user.value[key] = e.target.value
+  }
+</script>
+
+<template>
+  <form id="loginForm">
+    <div>
+      ID: <span >{{user.id}} </span>
+      <div>
+        <label for="user.id"><input type="text" @input="onInputHandler" id="user.id" /></label>
+      </div>
+    </div>
+    <div>
+      PASSWORD: <span>{{user.pw}} </span>
+      <div>
+        <label for="user.pw"><input type="password" @input="onInputHandler" id="user.pw"/></label>
+      </div>
+    </div>
+    <div>
+      SEX: <span>{{user.sex}} </span>
+      <div>
+        <label for="male"> <input type="radio" v-model="user.sex" id="male" value="male"/>male</label>
+        <label for="female"> <input type="radio" v-model="user.sex" id="female" value="female"/>female</label>
+      </div>
+    </div>
+
+    <button type="button" @click="login">LOGIN</button>
+  </form>
+</template>
+
+<style scoped>
+span {
+  font-weight: bold;
+}
+
+#loginForm {
+  margin: 10em;
+}
+</style>
