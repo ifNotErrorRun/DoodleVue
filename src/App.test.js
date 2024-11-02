ec003251bd8481272399fb33b89a9dac581d8de6
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-11-02 17:34:10 +0900

    feat: prettier, vue-router, pinia 기본 설정 추가

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import {describe, it, expect} from "vitest";
+import { describe, expect, it } from 'vitest'
 
 describe('App.vue', () => {
-    it('test 1 is 1', () => {
-        expect(1).toEqual(1);
-    })
+  it('test 1 is 1', () => {
+    expect(1).toEqual(1)
+  })
 })
